71198d7a0483d1fce0176d64e25e9df76195e9bf
Yuanzhuo Wang <ywang249@u.rochester.edu>
2019-03-15 17:34:41 -0400

    update Teaching, Rating

--- src/main/java/com/courscio/api/course/Rating.java
+++ src/main/java/com/courscio/api/course/Rating.java
@@ -2,10 +2,48 @@ package com.courscio.api.course;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * @author Jiupeng Zhang
  * @since 03/14/2019
  */
 public class Rating implements Serializable {
-    // todo
+	private Long id;
+	private Long user_id;
+	private Long course_id;
+	private Integer score;
+	private String comment;
+	
+	@Id
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+	public Long getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(Long course_id) {
+		this.course_id = course_id;
+	}
+	public Integer getScore() {
+		return score;
+	}
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 }
--- src/main/java/com/courscio/api/course/Teaching.java
+++ src/main/java/com/courscio/api/course/Teaching.java
@@ -2,10 +2,58 @@ package com.courscio.api.course;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * @author Jiupeng Zhang
  * @since 03/14/2019
  */
 public class Teaching implements Serializable {
-    // todo
+	private Long id;
+	private Long professor_id;
+	private Long course_id;
+	private String term;
+	private Integer capacity;
+	private String location;
+	
+	@Id
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getProfessor_id() {
+		return professor_id;
+	}
+	public void setProfessor_id(Long professor_id) {
+		this.professor_id = professor_id;
+	}
+	public Long getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(Long course_id) {
+		this.course_id = course_id;
+	}
+	public String getTerm() {
+		return term;
+	}
+	public void setTerm(String term) {
+		this.term = term;
+	}
+	public Integer getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
 }
