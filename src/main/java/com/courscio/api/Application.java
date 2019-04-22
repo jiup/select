13defbf1121f04274a055bc87c469b176ecf9d5f
Jh Zhang <jzhang91@u.rochester.edu>
2019-04-21 21:16:30 -0400

    try fixing cors

--- src/main/java/com/courscio/api/Application.java
+++ src/main/java/com/courscio/api/Application.java
@@ -151,7 +151,11 @@ public class Application extends SpringBootServletInitializer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("*")
+                    .allowCredentials(true);;
             }
         };
     }
